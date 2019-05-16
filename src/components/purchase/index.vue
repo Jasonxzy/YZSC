34ac27aa817e913f9e4ab8440b3a6757916e203f
loveYANG73 <1508576215@qq.com>
2019-05-16 09:10:45 +0800

    十六号杨明松一更

--- src/components/purchase/index.vue
+++ src/components/purchase/index.vue
@@ -1,28 +1,48 @@
 <template>
   <div>
-    <div class="Shopshow float-l">
-      <div class="Product">
-        <div class="productleft float-l">
-          <div class="shopimg">
-            <img src="" />放图片的
-          </div>
-          <div class="carousel">
-            <div class=""> <左移动 </div>
-            <div class=""> 中间图片 </div>
-            <div class=""> 右移动> </div>
-          </div>
-        </div>
-        <div class="productright float-r">商品信息文字</div>
-      </div>
-      <div class="ProductShow">下面的图片展示</div>
+    <div class="YZ-top">
+      <a href="">主页</a>
+      <a href="">元祖商城</a>
+      <a href="">冰品季</a>
+      <a href="">冰品点心</a>
+      <a href=""></a>
     </div>
-    <div class="Maylove float-r">右边的喜欢</div>
+    <div class="YZ-bottom">
+      <div class="Shopshow float-l">
+        <div class="Product">
+          <div class="productleft float-l">
+            <div class="shopimg">
+              <img src="" />放图片的
+            </div>
+            <div class="carousel">
+              <div class=""> <左移动 </div>
+              <div class=""> 中间图片 </div>
+              <div class=""> 右移动> </div>
+            </div>
+          </div>
+          <div class="productright float-r">商品信息文字</div>
+        </div>
+        <div class="ProductShow">下面的图片展示</div>
+       </div>
+      <div class="Maylove float-r">右边的喜欢</div>
+    </div>
+
   </div>
 </template>
 <script>
   export default {}
 </script>
 <style scoped>
+  .YZ-top{
+    width: 1199px;
+    height: 45px;
+    line-height: 45px;
+    margin: 0 auto;
+  }
+  .YZ-bottom{
+    width: 1199px;
+    margin: 0 auto;
+  }
   .Shopshow{
     width: 991px;
   }
@@ -32,6 +52,7 @@
   .Product{
     width: 991px;
     height: 600px;
+
   }
   .productleft{
     width: 477px;
